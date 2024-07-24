63a599ea1e6df6821e51669d579ae336b7f43266
Peter van Dijk <peter.van.dijk@bynder.com>
2024-07-24 23:24:24 +0200

    added model\

---
+++ dogschool_es/helpers/aggregate_to_model.py
@@ -0,0 +1,5 @@
+from dogschool_es.aggregates.trick_aggregate import Trick
+from dogschool_es.read_models.SchoolTricksReadModel import TricksWithDogs
+
+def trick_aggregate_to_model(trick: Trick) -> TricksWithDogs:
+    return TricksWithDogs(trick_id=trick.id, trick_name=trick.name, dog_names=trick.dog_names)
