a0fce1aefe1d88c51e76837db1fe72a100d86816
Peter van Dijk <peter.van.dijk@bynder.com>
2024-07-25 09:03:37 +0200

    updating read repo

--- dogschool_es/read_repositories/readrepository.py
+++ dogschool_es/read_repositories/readrepository.py
@@ -1,15 +1,20 @@
 from sqlalchemy.orm import Session
 
+from dogschool_es.read_models.tricks_read_model import TricksWithDogs
+
 class SchoolTricksRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_tricks(self):
-        return self.session.get_tricks()
+    # def get_tricks(self):
+    #     return self.session.get_tricks()
     
     def add_trick(self, trick):
-        # 
-        self.session.add(trick)
+        # check existing then update
+        try:
+            self.session.query(TricksWithDogs).filter(TricksWithDogs.trick_id == trick.trick_id).update({ "dog_names": trick.dog_names })
+        except:
+            self.session.add(trick)
         self.session.commit()
         
     # def update_trick(self, trick):
