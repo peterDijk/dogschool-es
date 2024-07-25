2923c02aaf87a88eeb3d1373c32f75274c86af0f
Peter van Dijk <peter.van.dijk@bynder.com>
2024-07-25 09:38:27 +0200

    datetime warning fixed

--- dogschool_es/read_models/tricks_read_model.py
+++ dogschool_es/read_models/tricks_read_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlalchemy import Column, DateTime, String
 
@@ -13,5 +13,5 @@ class TricksWithDogs(Base):
     trick_id = Column(String(255), primary_key=True)
     name = Column(String(255), nullable=False, unique=False)
     dog_names = Column(String(255), nullable=False)
-    date_added = Column(DateTime(timezone=True), default=datetime.utcnow)
-    date_modified = Column(DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
+    date_added = Column(DateTime(timezone=True), default=datetime.now(timezone.utc))
+    date_modified = Column(DateTime(timezone=True),  default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
--- dogschool_es/read_repositories/readrepository.py
+++ dogschool_es/read_repositories/readrepository.py
@@ -10,8 +10,11 @@ class SchoolTricksRepository:
     #     return self.session.get_tricks()
     
     def add_trick(self, trick):
-        # check existing then update
+        # check existing then update -> this should not be needed
+        # why doesn't it work to add in try/except block?
+        
         existing_entry = self.session.query(TricksWithDogs).filter(TricksWithDogs.trick_id == trick.trick_id).first()
+        
         if existing_entry is not None:
             self.session.query(TricksWithDogs).filter(TricksWithDogs.trick_id == trick.trick_id).update({ "dog_names": trick.dog_names })
             # self.session.flush()
