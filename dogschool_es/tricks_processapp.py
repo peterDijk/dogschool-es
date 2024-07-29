d168f54a19575d0cbeb112ffc7f4f1b2a685ce89
Peter van Dijk <39601927+peterDijk@users.noreply.github.com>
2024-07-29 21:46:30 +0200

    Change Tricks to Application with method. Not listening for Dog events anymore

--- dogschool_es/tricks_processapp.py
+++ dogschool_es/tricks_processapp.py
@@ -1,30 +1,20 @@
 import os
 from eventsourcing.application import AggregateNotFound
-from eventsourcing.system import ProcessApplication
+from eventsourcing.system import Application
 from eventsourcing.dispatch import singledispatchmethod
 from eventsourcing.domain import Aggregate, event
 
 from dogschool_es.aggregates.dog_aggregate import Dog
 from dogschool_es.aggregates.trick_aggregate import Trick
 
-class Tricks(ProcessApplication):
-    @singledispatchmethod
-    def policy(self, domain_event, process_event):
-        """Default policy"""
-
-    @policy.register(Dog.TrickAdded)
-    def _(self, domain_event, process_event):
-        event_trick_name = domain_event.trick
-        dog_id = domain_event.originator_id
-        dog = self.repository.get(dog_id)
-
+class Tricks(Application):
+    def add_trick(trick_name: str, dog: Dog):
         try:
-            trick_id = Trick.create_id(event_trick_name)
+            trick_id = Trick.create_id(trick_name)
             trick = self.repository.get(trick_id)
         except AggregateNotFound:
-            trick = Trick(event_trick_name)
+            trick = Trick(trick_name)
         trick.add_dog(dog.name)
-        process_event.collect_events(trick)
         self.save(trick)
         
     def get_trick(self, trick_name):
